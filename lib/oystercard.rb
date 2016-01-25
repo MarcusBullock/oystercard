78332d6e2aad7c2eec0cbab1dbcf97054f5fb44d
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-25 16:35:14 +0000

    Added empty code and spec files for Station class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,14 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :entry_station, :exit_station
 
   DEFAULT_BALANCE = 0
   MAX_LIMIT = 90
   MIN_FARE = 1
 
-  def initialize(balance=DEFAULT_BALANCE)
-    @balance = balance
-    @in_journey = false
+  def initialize
+    @balance = DEFAULT_BALANCE
+    @hist = []
   end
 
   def topup(value)
@@ -17,17 +17,24 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    !@entry_station.nil?
   end
 
-  def touch_in
+  def touch_in station
     fail 'low balance' if balance < MIN_FARE
-    @in_journey = true
+    @entry_station = station
+    @exit_station = nil
   end
 
-  def touch_out
+  def touch_out station
     deduct(MIN_FARE)
-    @in_journey = false
+    @exit_station = station
+    @hist << {entry_station => exit_station}
+    @entry_station = nil
+  end
+
+  def hist
+    @hist.clone
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,14 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:oystercard){described_class.new}
+      let (:station) {double(:station)}
+      let (:station2) {double(:station)}
 
   describe '#initialize' do
     it 'checks that the card has a balance' do
       expect(subject.balance).to be 0
-    end
-
-    it 'allows the user to set the initial balance' do
-      expect(Oystercard.new(5).balance).to eq 5
     end
   end
 
@@ -23,21 +21,15 @@ describe Oystercard do
     end
   end
 
-  # describe '#deduct' do
-  #   it 'allows the user to deduct a specified fare from the balance' do
-  #     expect{subject.deduct(5)}.to change{subject.balance}.by -5
-  #   end
-  # end
-
   describe '#touch in and out' do
-    before {subject.topup 5; subject.touch_in}
+    before {subject.topup 1; subject.touch_in(station)}
 
     it 'show that user is in journey when touched in' do
       expect(subject).to be_in_journey
     end
 
     it 'show that user is not in journey when touched out' do
-      subject.touch_out
+      subject.touch_out station
       expect(subject).not_to be_in_journey
     end
   end
@@ -45,14 +37,48 @@ describe Oystercard do
   context 'requiring minimum balance' do
 
     it 'fails when touch-in is attempted with low balance' do
-      expect{subject.touch_in}.to raise_error 'low balance'
+      expect{subject.touch_in(station)}.to raise_error 'low balance'
     end
 
     it 'deducts MIN_FARE on touch_out' do
       subject.topup(1)
-      subject.touch_in
-      subject.touch_out
+      subject.touch_in station
+      subject.touch_out station
       expect(subject.balance).to eq 0
+    end
+  end
+
+  context 'adding entry and exit stations' do
+    before {subject.topup(5); subject.touch_in(station)}
+    it 'stores the entry station' do
+      expect(subject.entry_station).to be station
+    end
+
+    it 'removes station on touchout' do
+      subject.touch_out station
+      expect(subject.entry_station).to be nil
+    end
+
+    it 'stores exit station' do
+      subject.touch_out station
+      expect(subject.exit_station).to be station
+  end
+
+    it 'removes exit station on touchin' do
+      expect(subject.exit_station).to be nil
+    end
+  end
+
+  context 'journey history' do
+    it 'journey history is empty' do
+      expect(subject.hist).to eq []
+    end
+
+    it 'adds one journey history per trip' do
+      subject.topup(80)
+      subject.touch_in station
+      subject.touch_out station2
+      expect(subject.hist).to eq [{station => station2}]
     end
   end
 end
