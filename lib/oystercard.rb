5bd7914bc0ce20d8af1519623ccf06f97640b378
Rufus Raghunath <rufusraghunath@gmail.com>
2016-01-25 15:40:46 +0000

    Added tests and code up to challenge10

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,8 +2,38 @@ class Oystercard
 
   attr_reader :balance
 
-  def initialize(balance=0)
+  DEFAULT_BALANCE = 0
+  MAX_LIMIT = 90
+  MIN_FARE = 1
+
+  def initialize(balance=DEFAULT_BALANCE)
     @balance = balance
+    @in_journey = false
+  end
+
+  def topup(value)
+    fail "Exceeded max limit: #{MAX_LIMIT}" if @balance + value >= MAX_LIMIT
+    @balance += value
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    fail 'low balance' if balance < MIN_FARE
+    @in_journey = true
+  end
+
+  def touch_out
+    deduct(MIN_FARE)
+    @in_journey = false
+  end
+
+  private
+
+  def deduct(value)
+    @balance -= value
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,6 +11,48 @@ describe Oystercard do
     it 'allows the user to set the initial balance' do
       expect(Oystercard.new(5).balance).to eq 5
     end
+  end
 
+  describe '#topup' do
+    it 'allows the user to add balance to their card' do
+      expect{subject.topup(5)}.to change{subject.balance}.by 5
+    end
+
+    it 'throws an error if the new balance exceeds limit' do
+      expect{subject.topup((Oystercard::MAX_LIMIT)+1)}.to raise_error "Exceeded max limit: #{Oystercard::MAX_LIMIT}"
+    end
+  end
+
+  # describe '#deduct' do
+  #   it 'allows the user to deduct a specified fare from the balance' do
+  #     expect{subject.deduct(5)}.to change{subject.balance}.by -5
+  #   end
+  # end
+
+  describe '#touch in and out' do
+    before {subject.topup 5; subject.touch_in}
+
+    it 'show that user is in journey when touched in' do
+      expect(subject).to be_in_journey
+    end
+
+    it 'show that user is not in journey when touched out' do
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
+
+  context 'requiring minimum balance' do
+
+    it 'fails when touch-in is attempted with low balance' do
+      expect{subject.touch_in}.to raise_error 'low balance'
+    end
+
+    it 'deducts MIN_FARE on touch_out' do
+      subject.topup(1)
+      subject.touch_in
+      subject.touch_out
+      expect(subject.balance).to eq 0
+    end
   end
 end
