9a5490da9eb795f65e640d95ffd045be1367a9ca
Eilw <eirikwiig@gmail.com>
2016-01-25 14:11:24 +0000

    finished ex4 - added balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,16 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:oystercard){described_class.new}
+
+  describe '#initialize' do
+    it 'checks that the card has a balance' do
+      expect(subject.balance).to be 0
+    end
+
+    it 'allows the user to set the initial balance' do
+      expect(Oystercard.new(5).balance).to eq 5
+    end
+
+  end
 end
