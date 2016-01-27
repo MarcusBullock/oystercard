156b24b2f2bd0416db49876634c5dbb075c8b58a
Marcus <marcusbullock@hotmail.co.uk>
2016-01-27 11:00:08 +0000

    Successfully implemented journey class

---
+++ lib/journey.rb
@@ -0,0 +1,18 @@
+class Journey
+
+  attr_reader :entry_station
+  PENALTY_FARE = 6
+  
+  def start_journey(entry_station)
+    @entry_station = entry_station
+  end
+
+  def end_journey(exit_station)
+    @exit_station = exit_station
+  end
+
+  def calculate_fare
+    @entry_station && @exit_station ? Oystercard::MIN_FARE : PENALTY_FARE
+  end
+
+end
---
+++ spec/journey_spec.rb
@@ -0,0 +1,36 @@
+require 'journey'
+
+describe Journey do
+
+  subject(:journey){described_class.new}
+  let(:entry_station){double(:station)}
+  let(:exit_station){double(:station)}
+
+  describe '#start_journey' do
+
+    it 'starts with an entry station' do
+      expect(subject.start_journey(entry_station)).to eq entry_station
+    end
+  end
+
+  describe '#end_journey' do
+
+    it 'ends a journey' do
+      expect(subject.end_journey(exit_station)).to eq exit_station
+    end
+  end
+
+  describe '#calculate_fare' do
+
+    it 'calculates a fare' do
+      subject.start_journey(entry_station)
+      subject.end_journey(exit_station)
+      expect(subject.calculate_fare).to eq Oystercard::MIN_FARE
+    end
+
+    it 'gives a penalty fare when you touch in twice' do
+      2.times{subject.start_journey(entry_station)}
+      expect(subject.calculate_fare).to eq Journey::PENALTY_FARE
+    end
+  end
+end
